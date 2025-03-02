430ffa37f2afab1dbc79e45f57a6b59b67ce4a78
Ylyas Abdywahytow <ylyasabdywahytow@Ylyass-MacBook-Pro.local>
2025-03-02 11:59:34 -0500

    Added Project

---
+++ Shoppe/App/ShoppeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppeApp.swift
+//  Shoppe
+//
+//  Created by Ylyas Abdywahytow on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
