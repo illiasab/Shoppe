6bd367c513c72e65e46090fdc84e9b3923d57ef6
Ylyas Abdywahytow <ylyasabdywahytow@Ylyass-MacBook-Pro.local>
2025-03-04 13:02:23 -0500

    Changed OnboardingView

--- Shoppe/Views/OnboardingView/OnboardingView.swift
+++ Shoppe/Views/OnboardingView/OnboardingView.swift
@@ -1,63 +1,108 @@
-//
-//  OnboardingView.swift
-//  Shoppe
-//
-//  Created by Ylyas Abdywahytow on 3/3/25.
-//
-
 import SwiftUI
 
 struct OnboardingView: View {
+    let pages = [
+        (image: "shoppingImage1", title: "Welcome!", description: "Discover a fast and easy way to shop online."),
+        (image: "shoppingImage2", title: "Smart Search & Favorites", description: "Find products instantly and save favorites for later."),
+        (image: "shoppingImage3", title: "Easy Checkout", description: "Add to cart, choose payment, and order in seconds."),
+        (image: "shoppingImage4", title: "Manage Your Store", description: "Become a manager, add products, and control your catalog!")
+    ]
     enum Event {
         case onboardingComplete
     }
     
     var didSendEventHandler: ((OnboardingView.Event) -> Void)?
     
-    // Define the page index for tabView
     @State private var currentPage = 0
     
     var body: some View {
-        VStack {
-            TabView(selection: $currentPage) {
-                ForEach(0..<4, id: \.self) { index in
-                    VStack {
-                        Text("Page \(index + 1)")
-                            .font(.largeTitle)
-                            .padding()
-                        Image(systemName: "star.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(.blue)
-                            .padding()
-                        Text("This is slide \(index + 1) content.")
-                            .font(.body)
-                            .padding()
+        GeometryReader { geometry in
+            
+            Image("bubble2")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 400, height: 400)
+                .padding(.leading, -100)
+                .padding(.top, 250)
+            
+            Image("bubble1")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 400, height: 400)
+                .padding(.leading, -100)
+                .padding(.top, -100)
+            Spacer()
+            ZStack {
+                VStack {
+                    TabView(selection: $currentPage) {
+                        ForEach(0..<pages.count, id: \ .self) { index in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .fill(Color.white)
+                                    .shadow(radius: 5)
+                                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.85)
+                                
+                                VStack {
+                                    Image(pages[index].image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.50)
+                                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous).path(in: CGRect(x: 0, y: 0, width: geometry.size.width * 0.8, height: geometry.size.height * 1)))
+                                        //.cornerRadius(20)
+                                        .padding(.top, -100)
+                                    Spacer()
+                                    Spacer()
+                                    Text(pages[index].title)
+                                        .font(.custom("Raleway-Bold", size: 28))
+                                        .bold()
+                                        .padding(.top, 10)
+                                        .padding(.horizontal, 20)
+                                        .multilineTextAlignment(.center)
+                                    Spacer()
+                                    Text(pages[index].description)
+                                        .font(.custom("NunitoSans-VariableFont_YTLC,opsz,wdth,wght", size: 17))
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.horizontal, 40)
+                                    Spacer()
+                                    if index == pages.count - 1 {
+                                        Button(action: {didSendEventHandler?(.onboardingComplete)}) {
+                                            Text("Начать")
+                                                .font(.custom("NunitoSans-VariableFont_YTLC,opsz,wdth,wght", size: 22))
+                                                .foregroundColor(.white)
+                                                .frame(width: 200, height: 50)
+                                                .background(Color.mainBlue)
+                                                .cornerRadius(15)
+                                        }
+                                        .padding(.bottom, -100)
+                                    }
+                                }
+                                .frame(width: geometry.size.width * 0.85, height: geometry.size.height * 0.6)
+                            }
+                            .tag(index)
+                        }
                     }
-                    .tag(index)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    
+                    HStack(spacing: 8) {
+                        ForEach(0..<pages.count, id: \ .self) { index in
+                             
+                            Circle()
+                                .fill(index == currentPage ? Color.mainBlue : Color.mainBlue.opacity(0.2))
+                                .frame(width: 20, height: 20)
+                        }
+                    }
+                    .padding(.top, -20)
+                    
+                    Spacer()
                 }
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            .frame(height: 300)
-            
-            Button(action: {
-                print("✅ отправляем событие launchComplete")
-                didSendEventHandler?(.onboardingComplete)
-            }) {
-                Text("Complete Onboarding")
-                    .font(.headline)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .padding(.top, 20)
         }
-        .padding()
     }
 }
 
-#Preview {
-    OnboardingView()
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
 }
---
+++ Shoppe/Views/OnboardingView/OnboardingViewMock.swift
@@ -0,0 +1,63 @@
+//
+//  OnboardingView.swift
+//  Shoppe
+//
+//  Created by Ylyas Abdywahytow on 3/3/25.
+//
+//
+//import SwiftUI
+//
+//struct OnboardingViewMock: View {
+////    enum Event {
+////        case onboardingComplete
+////    }
+////    
+////    var didSendEventHandler: ((OnboardingViewMock.Event) -> Void)?
+//    
+//    // Define the page index for tabView
+//    @State private var currentPage = 0
+//    
+//    var body: some View {
+//        VStack {
+//            TabView(selection: $currentPage) {
+//                ForEach(0..<4, id: \.self) { index in
+//                    VStack {
+//                        Text("Page \(index + 1)")
+//                            .font(.largeTitle)
+//                            .padding()
+//                        Image(systemName: "star.fill")
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(width: 100, height: 100)
+//                            .foregroundColor(.blue)
+//                            .padding()
+//                        Text("This is slide \(index + 1) content.")
+//                            .font(.body)
+//                            .padding()
+//                    }
+//                    .tag(index)
+//                }
+//            }
+//            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+//            .frame(height: 300)
+//            
+//            Button(action: {
+//                print("✅ отправляем событие launchComplete")
+//                didSendEventHandler?(.onboardingComplete)
+//            }) {
+//                Text("Complete Onboarding")
+//                    .font(.headline)
+//                    .padding()
+//                    .background(Color.blue)
+//                    .foregroundColor(.white)
+//                    .cornerRadius(10)
+//            }
+//            .padding(.top, 20)
+//        }
+//        .padding()
+//    }
+//}
+//
+//#Preview {
+//    OnboardingViewMock()
+//}
