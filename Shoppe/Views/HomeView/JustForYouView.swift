3320aabd507b02f89758e33db5089d05d5729817
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-04 16:29:17 +0300

    //add homeView/logic/

---
+++ Shoppe/Views/HomeView/JustForYouView.swift
@@ -0,0 +1,81 @@
+//
+//  JustForYouView.swift
+//  Shoppe
+//
+//  Created by Marat Fakhrizhanov on 04.03.2025.
+//
+
+import SwiftUI
+
+struct JustForYouView: View {
+    let priceRegion: String
+    let products: [Product]
+    let addInCartaction: () -> Void
+    let addInFavorites: () -> Void
+    
+    var body: some View {
+        ScrollButtons(title: "Just For You", button: false, action: {})
+            .padding(.horizontal,20)
+        
+        LazyVGrid(columns: [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)]) {
+            ForEach(products, id:\.id) { product in
+                VStack(alignment: .leading, spacing: 7) {
+                    VStack(alignment: .leading) {
+                        // IMAGE
+                        AsyncImage(url: URL(string: product.imageUrl)) { Image in
+                            Image
+                                .resizable()
+                                .scaledToFill()
+                                .clipShape(RoundedRectangle(cornerRadius: 9))
+                        } placeholder: {
+                            Image("PlaceholderProduct")
+                                .resizable()
+                                .scaledToFill()
+                                .clipShape(RoundedRectangle(cornerRadius: 9))
+                        }
+                        //TEXT
+                        Text(product.description)
+                            .applyFont(.nunitoSans,.light,12)
+                            .lineLimit(2)
+                            .frame(height: 30)
+                    }
+                    .onTapGesture {
+                        // go to Product INFO view
+                        print("go to productinfo view ( product id: \(product.id)")
+                    }
+                    HStack {
+                        Text("\(Double(product.price) ?? 0.00, format: .currency(code: priceRegion))")
+                            .applyFont(.raleway,.bold,17)
+                        Spacer()
+                        Button {
+                            addInFavorites()
+                        } label: {
+                            Image(systemName: "heart") // add favorite  in  model
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.favoriteRed)
+                                .frame(width: 20)
+                        }
+                    }
+                    Button {
+                        addInCartaction()
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundStyle(.mainBlue)
+                                .frame(height: 30)
+                            Text("Add to cart")
+                                .applyFont(.inter,.regular, 12)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    JustForYouView(priceRegion: "RUR", products: [Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur")], addInCartaction: {print("in cart")}, addInFavorites: {print("in favorit")})
+}
---
+++ Shoppe/Views/HomeView/PopularProducts.swift
@@ -0,0 +1,66 @@
+//
+//  PopularItemsView.swift
+//  Shoppe
+//
+//  Created by Marat Fakhrizhanov on 03.03.2025.
+//
+
+import SwiftUI
+
+struct PopularProducts: View {
+    let priceRegion: String
+    let products: [Product]
+    let action: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            ScrollButtons(title: "Popular", button: true, action: {action()})
+                .padding(.horizontal,20)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 10) {
+                    ForEach(products, id:\.id) { product in
+                        VStack {
+                            // IMAGE
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 9)
+                                    .foregroundStyle(.white)
+                                    .shadow(radius: 5)
+                                
+                                AsyncImage(url: URL(string: product.imageUrl)) { Image in
+                                    Image
+                                        .resizable()
+                                        .scaledToFill()
+                                        .padding(5)
+                                } placeholder: {
+                                    Image("adidasPopular")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .padding(5)
+                                }
+                            }
+                            .frame(width: 140, height: 140)
+                            // TEXT
+                            VStack(alignment: .leading){
+                                Text(product.description)
+                                    .applyFont(.nunitoSans,.regular,12)
+                                    .lineLimit(2)
+                                    .frame(height: 30)
+                                
+                                Text("\(Double(product.price) ?? 0.00, format: .currency(code: priceRegion))")
+                                    .applyFont(.raleway,.bold,17)
+                            }
+                            .frame(width: 140)
+                        }
+                    }
+                }
+                .padding(.leading,20)
+            }
+        }
+        .frame(height: 247)
+    }
+}
+
+#Preview {
+    PopularProducts(priceRegion: "EUR", products: [Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: "Lorem ipsum dolor sit amet consectetur."), Product(title: "Adidas", price: "99", imageUrl: "", description: ""), Product(title: "Adidas", price: "99", imageUrl: "", description: "")], action: {})
+}
