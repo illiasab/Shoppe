b19461b2597ca6fa70f5e9a0524755aa6300a0c2
Daniil Pavlenko <danyy.pavl@gmail.com>
2025-03-08 17:56:58 +0300

    add tabView

--- Shoppe/App/ShoppeApp.swift
+++ Shoppe/App/ShoppeApp.swift
@@ -10,16 +10,82 @@ import Firebase
 @main
 struct ShoppeApp: App {
     @StateObject var dependencies = Dependencies()
+    @State var selectedTab: TabType = .home
 
     init(){
         FirebaseApp.configure()
+        setupTabBarAppearance()
     }
     
     var body: some Scene {
         WindowGroup {
-            CoordinatorView(dependencies: dependencies)
-                .edgesIgnoringSafeArea(.all)
+            TabView() {
+                CoordinatorView(dependencies: dependencies)
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        Image("Shopz")
+                            .renderingMode(.template)
+                    }
+                    .tag(TabType.home)
+                
+                CoordinatorView(dependencies: dependencies)
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        Image("heartz")
+                            .renderingMode(.template)
+                    }
+                    .tag(TabType.favorites)
+                
+                CoordinatorView(dependencies: dependencies)
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        Image("Categoriesz")
+                            .renderingMode(.template)
+                    }
+                    .tag(TabType.category)
+                
+                CoordinatorView(dependencies: dependencies)
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        Image("cartz")
+                            .renderingMode(.template)
+                    }
+                    .tag(TabType.cart)
+                
+                CoordinatorView(dependencies: dependencies)
+                    .edgesIgnoringSafeArea(.all)
+                    .tabItem {
+                        Image("human")
+                            .renderingMode(.template)
+                    }
+                    .tag(TabType.profile)
+            }
         }
+    }
+    
+    private func setupTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+
+        appearance.stackedLayoutAppearance.selected.iconColor = .black
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.black]
+
+        appearance.stackedLayoutAppearance.normal.iconColor = .blue
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.mainBlue]
+
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().tintColor = .black
+        UITabBar.appearance().unselectedItemTintColor = .mainBlue
+    }
+    
+    enum TabType {
+        case home
+        case favorites
+        case category
+        case profile
+        case cart
     }
 }
 
